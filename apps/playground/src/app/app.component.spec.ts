c31d2bbc2f0a35c5e45f77609600c828f76261c1
Samuel Ruiz <samuel.ruiz@atalantago.com>
2021-09-01 16:06:54 +0200

    Fix - testing

--- apps/playground/src/app/app.component.spec.ts
+++ apps/playground/src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from './shared/shared.module';
+import { MaterialModule } from '@ballet/material';
+import { BulmaModule } from '@ballet/bulma';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule,SharedModule, MaterialModule, BulmaModule, HttpClientModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
