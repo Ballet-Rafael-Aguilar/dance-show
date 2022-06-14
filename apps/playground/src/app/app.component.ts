30247170dfd4764961b2c477cf3543328b27b846
Samuel RUIZ (contractor) <samuel.ruiz@amadeus.com>
2022-06-14 10:12:27 +0200

    Chore

--- apps/playground/src/app/app.component.ts
+++ apps/playground/src/app/app.component.ts
@@ -4,8 +4,11 @@ import { ThemingService } from '@ballet/shared/theme';
 
 @Component({
   selector: 'ballet-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+      <div [ngClass]="selectedTheme$ | async">
+          <router-outlet></router-outlet>
+      </div>
+  `
 })
 export class AppComponent implements OnInit {
   selectedTheme$: Observable<string>;
